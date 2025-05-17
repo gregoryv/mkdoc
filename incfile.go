a6cccc786cc9694499e9d14d708dd0d74b206571
Gregory Vincic <gregoryvse@gmail.com>
2025-05-17 08:00:51 +0200

    Simplify incfile

--- incfile.go
+++ incfile.go
@@ -2,81 +2,31 @@ package txtfmt
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-var (
-	OPEN   byte = '<'
-	CLOSE  byte = '>'
-	delim       = string(OPEN) + string(CLOSE)
-	indent      = ""
-	tag         = "incfile"
-	prefix      = []byte(tag + " ")
-)
+func incfile(w io.Writer, r io.Reader, delim string) {
+	s := bufio.NewScanner(r)
+	prefix := "<incfile "
+	for s.Scan() {
+		line := s.Text()
 
-func incfile(w io.Writer, txt io.Reader, delim string) {
-	if len(delim) == 2 {
-		OPEN = delim[0]
-		CLOSE = delim[1]
-	}
-
-	r := bufio.NewReader(txt)
-	var err error
-	next := openTag
-	for {
-		next, err = next(w, r)
-		if err != nil {
-			break
+		if strings.HasPrefix(line, prefix) {
+			f := line[len(prefix):]
+			f = strings.TrimRight(f, ">")
+			f = strings.TrimSpace(f)
+			fh, err := os.Open(f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			io.Copy(w, fh)
+			fh.Close()
+		} else {
+			fmt.Fprintln(w, line)
 		}
 	}
 }
-
-func openTag(w io.Writer, r *bufio.Reader) (parseFn, error) {
-	// look for open character
-	head, err := r.ReadBytes(OPEN)
-	if err != nil {
-		w.Write(head)
-		return nil, err
-	}
-
-	// write everything before OPEN
-	if l := len(head); l > 1 {
-		w.Write(head[:l-1])
-		head = head[l-1:]
-	}
-
-	tail, err := r.ReadBytes(CLOSE)
-
-	// ignore other than our prefix
-	if !bytes.HasPrefix(bytes.TrimSpace(tail), prefix) {
-		w.Write(head)
-		w.Write(tail)
-		return openTag, nil // start over
-	}
-
-	// parse filename and arguments
-	tail = bytes.TrimSpace(tail)
-	filename := tail[len(prefix) : len(tail)-1]
-
-	// include the file
-	fh, err := os.Open(string(filename))
-	if err != nil {
-		handleError("incfile: ", err)
-		return openTag, nil
-	}
-	s := bufio.NewScanner(fh)
-	for s.Scan() {
-		fmt.Fprint(w, indent, s.Text(), "\n")
-	}
-	fh.Close()
-
-	return openTag, nil
-}
-
-type parseFn func(io.Writer, *bufio.Reader) (parseFn, error)
-
-var handleError = log.Fatal
