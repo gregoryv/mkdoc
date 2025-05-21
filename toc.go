6c95e017d97d4d0184abdbfeefc6e56455495487
Gregory Vincic <gregoryvse@gmail.com>
2025-05-21 05:38:29 +0200

    Refactor

---
+++ toc.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// parsetoc does two things; writes named sections to w and the table
+// of contents to toc.
+func parsetoc(w, toc io.Writer, r io.Reader, width int) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		line := s.Text()
+		if !strings.HasPrefix(line, "§") {
+			fmt.Fprintln(w, line)
+			continue
+		}
+		// find section identifier
+		i := strings.Index(line, " ")
+		if i == -1 {
+			continue
+		}
+		s := strings.TrimLeft(line[:i], "§")
+
+		h := strings.TrimSpace(line[i:])
+		// print section link to stderr
+		// three spaces separation as found in example RFC's
+		fmt.Fprintf(toc,
+			`<a href="#section-%s">%s</a>   %s`, s, s, h,
+		)
+		// fill remainig width using dots
+		n := width - len(s) - len(h) - 5
+		dots := strings.Repeat(".", n)
+		fmt.Fprintln(toc, " ", dots)
+
+		// print toc link to toc
+		format := `<a name="section-%s" href="#section-%s">%s</a> %s`
+		fmt.Fprintf(w, format, s, s, s, h)
+		fmt.Fprintln(w)
+	}
+}
+
+func inserttoc(w io.Writer, r, toc io.Reader) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		line := s.Text()
+		if strings.ToLower(line) == "table of contents" {
+			fmt.Fprintln(w, line)
+			fmt.Fprintln(w)
+			io.Copy(w, toc)
+		} else {
+			fmt.Fprintln(w, line)
+		}
+	}
+}
